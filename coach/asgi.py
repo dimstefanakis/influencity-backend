7d5c681d3325c20341f09f3820b6b1c52ddcbbf6
dim-stef <jimstef@outlook.com>
2021-03-12 21:21:12 +0200

    changed asgi app

--- coach/asgi.py
+++ coach/asgi.py
@@ -1,15 +1,18 @@
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coach.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from .channelsmiddleware import JWTChannelMiddleware
 import chat.routing
 import posts.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coach.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": JWTChannelMiddleware(
         URLRouter([
             chat.routing.websocket_urlpatterns[0],
