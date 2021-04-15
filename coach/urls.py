391dfc615e61fcf6e73966422201451d4e4774a1
dim-stef <jimstef@outlook.com>
2021-04-15 03:19:27 +0300

    bug fix

--- coach/urls.py
+++ coach/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
+    path('', include('django.contrib.auth.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
